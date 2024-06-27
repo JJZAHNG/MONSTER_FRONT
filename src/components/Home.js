48d82fb2a6411d4cab493cb540e6e6dd97f96c86
JJZAHNG <Jeazhang828@sina.com>
2024-06-27 20:12:27 +0800

    Home Updating

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,32 +1,105 @@
 import React from 'react';
-import { Layout, Menu, Typography } from 'antd';
-import { UserOutlined } from '@ant-design/icons';
+import { Layout, Menu, Avatar, Typography, Card, List } from 'antd';
+import {
+  UserOutlined,
+  LaptopOutlined,
+  NotificationOutlined,
+} from '@ant-design/icons';
 
-const { Header, Content, Footer, Sider } = Layout;
+const { Header, Content, Sider, Footer } = Layout;
 const { Title, Text } = Typography;
 
-const Home = ({ user }) => {
+const userData = {
+  username: 'ADMIN',
+  phone_number: '15105187807',
+  membership_level: 'Senior Ultra',
+};
+
+const courseData = [
+  {
+    title: 'Skill Training 1',
+    description: 'Description of skill training course 1',
+  },
+  {
+    title: 'Skill Training 2',
+    description: 'Description of skill training course 2',
+  },
+  {
+    title: 'Camp 1',
+    description: 'Description of camp 1',
+  },
+  {
+    title: 'Camp 2',
+    description: 'Description of camp 2',
+  },
+];
+
+const Home = () => {
   return (
     <Layout style={{ minHeight: '100vh' }}>
-      <Sider collapsible>
+      <Header className="header">
         <div className="logo" />
-        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-          <Menu.Item key="1" icon={<UserOutlined />}>
-            Home
-          </Menu.Item>
-          {/* Add more menu items here */}
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+          <Menu.Item key="1">Home</Menu.Item>
+          <Menu.Item key="2">Profile</Menu.Item>
+          <Menu.Item key="3">Settings</Menu.Item>
         </Menu>
-      </Sider>
-      <Layout className="site-layout">
-        <Header className="site-layout-background" style={{ padding: 0 }} />
-        <Content style={{ margin: '0 16px' }}>
-          <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
-            <Title level={2}>Welcome, {user.username}!</Title>
-            <Text>Your phone number: {user.phone_number}</Text>
-            {/* Add more user information here */}
-          </div>
-        </Content>
-        <Footer style={{ textAlign: 'center' }}>Your App ©2024 Created by You</Footer>
+      </Header>
+      <Layout>
+        <Sider width={200} className="site-layout-background">
+          <Menu
+            mode="inline"
+            defaultSelectedKeys={['1']}
+            style={{ height: '100%', borderRight: 0 }}
+          >
+            <Menu.Item key="1" icon={<UserOutlined />}>
+              Dashboard
+            </Menu.Item>
+            <Menu.Item key="2" icon={<LaptopOutlined />}>
+              Courses
+            </Menu.Item>
+            <Menu.Item key="3" icon={<NotificationOutlined />}>
+              Notifications
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout style={{ padding: '0 24px 24px' }}>
+          <Content
+            className="site-layout-background"
+            style={{
+              padding: 24,
+              margin: 0,
+              minHeight: 280,
+            }}
+          >
+            <Card style={{ marginBottom: 24 }}>
+              <Title level={2}>Welcome, {userData.username}!</Title>
+              <Avatar size={64} icon={<UserOutlined />} />
+              <Text style={{ display: 'block', marginTop: 16 }}>
+                Phone Number: {userData.phone_number}
+              </Text>
+              <Text>Membership Level: {userData.membership_level}</Text>
+            </Card>
+            <Card>
+              <Title level={3}>Your Courses</Title>
+              <List
+                itemLayout="horizontal"
+                dataSource={courseData}
+                renderItem={(item) => (
+                  <List.Item>
+                    <List.Item.Meta
+                      title={<a href="https://ant.design">{item.title}</a>}
+                      description={item.description}
+                    />
+                  </List.Item>
+                )}
+              />
+            </Card>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>
+            Monster System ©2024 Created by You
+          </Footer>
+        </Layout>
       </Layout>
     </Layout>
   );
